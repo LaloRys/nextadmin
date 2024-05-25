330cb1af205ade411af6e8bd95c446d028c3f022
DevRys <eduardoreyes55@aragon.unam.mx>
2024-05-24 22:10:25 -0600

    Create component Foote

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import Sidebar from '../ui/dashboard/sidebar/sidebar'
 import Navbar from '../ui/dashboard/navbar/Navbar'
 import styles from '../ui/dashboard/dashboard.module.css'
+import Footer from '../ui/dashboard/footer/Footer'
 
 function layout({ children }) {
   return (
     <div className={styles.container}>
-      <div className={styles.menu}><Sidebar/></div>
+      <div className={styles.menu}>
+        <Sidebar />
+      </div>
       <div className={styles.content}>
-        <Navbar/>
+        <Navbar />
         {children}
+        <Footer/>
       </div>
     </div>
   )
---
+++ app/ui/dashboard/footer/Footer.jsx
@@ -0,0 +1,15 @@
+
+import styles from './footer.module.css'
+
+function Footer() {
+  
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Rys Dev</div>
+      <div className={styles.text}>© All rights reserved.</div>
+      
+    </div>
+  )
+}
+
+export default Footer
