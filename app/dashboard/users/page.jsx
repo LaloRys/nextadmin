68fa3dd788157e0c26b05b7bf7600f3997951f57
DevRys <eduardoreyes55@aragon.unam.mx>
2024-05-24 23:23:43 -0600

    Create component Search & Users page

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import Search from '@/app/ui/dashboard/search/Search'
+import styles from '@/app/ui/dashboard/users/users.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 function UsersPage() {
   return (
-    <div>UsersPage</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Create At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                Jay Wilkerson
+              </div>
+            </td>
+            <td>tuov@ko.cm</td>
+            <td>13.01.2022</td>
+            <td>Admin</td>
+            <td>active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>View</button>
+                </Link>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.delete}`}>View</button>
+                </Link>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
